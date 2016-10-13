1b20471dfe2d035a5ca18e076a8ea030f767bf9d
卓文理 <531840344@qq.com>
2016-10-13 23:34:57 +0800

    use generator

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@
 
 const {parse} = require('url');
 
-export default (options = {}) => {
+module.exports = (options = {}) => {
   const logger = getLogger(options)
 
-  return async (ctx, next) => {
+  return function * (ctx, next) {
     const headers = ctx.req.headers
     const url = ctx.req.url
     const method = ctx.req.method
@@ -14,19 +14,19 @@ export default (options = {}) => {
     if (method !== 'GET') {
       logger(`${skip} because the method is not GET.`)
 
-      return await next()
+      yield * next
     } else if (!headers || typeof headers.accept !== 'string') {
       logger(`${skip} because the client did not send an HTTP accept header.`)
 
-      return await next()
+      yield * next
     } else if (headers.accept.indexOf('application/json') === 0) {
       logger(`${skip} because the client prefers JSON.`)
 
-      return await next()
+      yield * next
     } else if (!acceptsHtml(headers.accept)) {
       logger(`${skip} because the client does not accept HTML.`)
 
-      return await next()
+      yield * next
     }
 
     const parsedUrl = parse(url)
@@ -41,21 +41,21 @@ export default (options = {}) => {
         rewriteTarget = evaluateRewriteRule(parsedUrl, match, rewrite.to)
         logger(`Rewriting ${method} ${url} to ${rewriteTarget}`)
         ctx.req.url = rewriteTarget
-        return await next()
+        yield * next
       }
     }
 
     if (parsedUrl.pathname.includes('.')) {
       logger(`${skip} because the path includes a dot (.) character.`)
 
-      return await next()
+      yield * next
     }
 
     rewriteTarget = options.index || '/index.html'
     logger(`Rewriting ${method} ${url} to ${rewriteTarget}`)
     ctx.req.url = rewriteTarget
 
-    return await next()
+    yield * next
   };
 };
 
